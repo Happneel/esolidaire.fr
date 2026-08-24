a137a607d02d778592ec22d72b7570c20bef4bed
Happneel <69533322+Happneel@users.noreply.github.com>
2026-08-24 22:04:30 +0200

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import CommuneSelector from "@/components/CommuneSelector";
 
 export default function HomePage() {
   return (
-    <p1>pardon lucas mon bebou, viens jouer à fortnite avec nous tous solidaire ❤️</p1>
+    <h1 className="text-2xl font-bold text-slate-900 sm:text-3xl">
+        pardon lucas mon bebou, viens jouer à fortnite avec nous tous solidaire ❤️
+      </h1>
+
   );
 }
